c6fd38ac1c5c2097d792d942bb545715ee530dda
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-19 15:11:43 -0400

    Spring MVC

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
- @RequestMapping("/home")
+    @RequestMapping("/home")// if you do not specify, it will be added as default to initial page localhost:8080
     public String home(){
         return "home.html";
     }
+
+    @RequestMapping({"/apple","/banana"})// CAN'T have two end points with same name in different methods,
+    public String home2(){                    //because server will not know which one to choose
+        return "home.html";
+    }
+
 }
