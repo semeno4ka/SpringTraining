9f9a02863e21e3c248f8dc8e33daea82c3f56a22
Ekaterina <ekaterina.sementeva@gmail.com>
2022-09-26 15:35:23 -0400

    ThymeLeaf Form Implementation

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,19 +1,36 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")// will be concatinated to every end point
 public class MentorController {
 
 
-    @RequestMapping("/register")//localhost:8080/register
-    public String register(){
-        return "student/register";
+    @GetMapping("/register")//localhost:8080/register
+    public String register(Model model) {
+        List<String> batchList= Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+        model.addAttribute("mentor", new Mentor());
+        return "mentor/mentor-register";
     }
-    @RequestMapping("/drop")//localhost:8080/register
-    public String drop(){
-        return "student/drop";
-    }
+
+@PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+//how to access "mentor" attribute in this method
+
+        return "mentor/mentor-confirmation";
+}
+
+
+
 }
