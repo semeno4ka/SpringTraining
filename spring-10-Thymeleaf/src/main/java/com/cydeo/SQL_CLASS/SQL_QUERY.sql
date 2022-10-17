f9901b5a1a645c8faf2eafecd9bdeaf48f3b2a19
Ekaterina <ekaterina.sementeva@gmail.com>
2022-10-17 13:30:37 -0400

    SQL JOIN

--- spring-10-Thymeleaf/src/main/java/com/cydeo/SQL_CLASS/SQL_QUERY.sql
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/SQL_CLASS/SQL_QUERY.sql
@@ -101,3 +101,82 @@ CREATE VIEW emaillist AS select substr(first_name,1,1)||'.'|| substr(last_name,1
 Select* from emaillist;-- will bring you the saved email list
 --how to drop? rightclick the view and drop or
 DROP VIEW emaillist;
+
+select*from employees;
+select*from departments;
+select*from locations;
+--display firstname, lastname, departmentname
+select first_name,last_name,department_id from employees;
+
+SELECT first_name,last_name,department_name FROM employees e JOIN departments d ON e.department_id=d.department_id;-- gives only those who have departmetn
+SELECT first_name,last_name,department_name FROM employees e LEFT OUTER JOIN departments d ON e.department_id=d.department_id;-- gives all, no matter have or have not department assigned
+SELECT first_name,last_name,department_name FROM employees e LEFT OUTER JOIN departments d ON e.department_id=d.department_id WHERE d.department_id is Null;-- will bring those without assigned dep
+--get names, depName,city for all
+select first_name,last_name,department_name,city from employees e JOIN departments d on e.department_id=d.department_id
+                                                                  JOIN locations l on d.location_id = l.location_id;-- just add JOIN and continue to work with multiple tables
+
+select first_name,last_name,department_name,city, country_name from employees e JOIN departments d on e.department_id=d.department_id
+                                                                                JOIN locations l on d.location_id = l.location_id JOIN countries c on l.country_id = c.country_id;
+
+--SELF JOIN INTERVIEW QUESTION
+select e1.employee_id, e1.first_name,e1.last_name,e1.manager_id,e2.employee_id,e2.first_name,e2.last_name FROM employees e1 JOIN employees e2 ON e1.manager_id=e2.employee_id;--only those who has managers (Steven King doesn't)
+select e1.employee_id, e1.first_name,e1.last_name,e1.manager_id,e2.employee_id,e2.first_name,e2.last_name FROM employees e1 LEFT JOIN employees e2 ON e1.manager_id=e2.employee_id;--all employees
+
+select*from customer;
+select*from address;
+
+select first_name, last_name, address, phone from customer JOIN address ON customer.address_id = address.address_id;
+select first_name, last_name, address, phone from customer LEFT OUTER JOIN address ON customer.address_id = address.address_id;select first_name, last_name, address, phone from customer RIGHT OUTER JOIN address ON customer.address_id = address.address_id;
+select first_name, last_name, address, phone from customer FULL OUTER JOIN address ON customer.address_id = address.address_id;
+select first_name, last_name, address, phone from customer FULL OUTER JOIN address ON customer.address_id = address.address_id where customer.address_id IS NULL or address.address_id IS NULL;-- excludes intersection results
+-- you can put tableName + space + the first letter of the table to use a short version of table name it will be read as tableName as 'any letter'
+select first_name, last_name, customer.address_id,address.address_id, phone from customer FULL OUTER JOIN address ON customer.address_id = address.address_id;
+select first_name, last_name, c.address_id,a.address_id, phone from customer c FULL OUTER JOIN address a ON c.address_id = a.address_id;
+
+--creating our own tables. Commands work just one time
+-- create tableName ( colN1 DataType Constraints, cilN2 DataType Constraints(optional), colN3 DataType Constraints)
+
+CREATE TABLE ScrumTeam(
+                          empl_id Integer PRIMARY KEY,-- first column
+                          first_name varchar (30) Not NULL,-- (30)- how many characters can be accepted
+                          last_name varchar(30),
+                          job_title varchar(20)
+);
+Select*from scrumTeam;
+--insert information
+-- INSERT INTO tableName(colN1,colN2..) VALUES (value1,value2)
+INSERT INTO ScrumTeam(empl_id, first_name, last_name, job_title) -- mandatory are those, which have Not Null and PrimaryKey constraints
+VALUES(1,'Severus','Snape','Tester');-- in order to check whether the input was successful, you have to select*from
+INSERT INTO ScrumTeam VALUES(2,'Harry','Potter','Developer');-- if you do not specify columns, it will understand that you fill out all of them
+INSERT INTO ScrumTeam VALUES (3,'Phoebe','Buffay','ScrumMaster');
+insert into ScrumTeam VALUES (4,'Michael','Jackson','PO');
+
+--How to UPDATE tables?
+--UPDATE tableName SET colN1 = value1, colN2 = value2,... WHERE condition...;
+UPDATE ScrumTeam SET job_title='DevOps' where empl_id=4;-- to check changes, select*from
+
+
+--How to DELETE
+-- DELETE FROM tableName  WHERE condition;
+DELETE FROM ScrumTeam WHERE empl_id=3;
+
+commit;-- to save data after changes, checkpoint
+Select*from scrumTeam;
+--How to add a column or change existing column name?
+--ALTER TABLE tableName action;
+--Add
+ALTER TABLE scrumteam ADD salary Integer;-- add column, value null
+UPDATE ScrumTeam SET salary=12000 WHERE empl_id=1;
+UPDATE ScrumTeam SET salary=10000 WHERE empl_id=2;
+UPDATE ScrumTeam SET salary=11000 WHERE empl_id=4;
+--Change column name
+ALTER TABLE scrumteam RENAME COLUMN salary TO annual_salary;
+
+--Delete/drop the full column
+ALTER TABLE scrumteam DROP COLUMN annual_salary;
+--Rename table
+ALTER TABLE scrumteam RENAME TO agileTeam;-- all the precious commands will give error, because it is no longer existing table
+select*from agileTeam;
+--How to delete all the data? TRUNCATE and DROP
+TRUNCATE TABLE agileTeam;--table exists, no data inside
+DROP TABLE agileTeam;-- the entire table is deleted
