0416ae53cdf8b3c2c82a4c735ea76021ab060b21
Ekaterina <ekaterina.sementeva@gmail.com>
2022-10-18 12:22:07 -0400

    trigger

--- spring-10-Thymeleaf/src/main/java/com/cydeo/SQL_CLASS/SQL_QUERY.sql
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/SQL_CLASS/SQL_QUERY.sql
@@ -338,3 +338,52 @@ END
 $$;
 call update_jobTitle_by_id(4,'Spring Developer');
 select*from scrumteam;
+
+--TRIGGER
+DROP TABLE IF EXISTS mentors;
+CREATE TABLE mentors(
+                        id INT GENERATED ALWAYS AS IDENTITY,
+                        first_name varchar(40) not null,
+                        last_name varchar(40) not null ,
+                        primary key(id)
+);
+
+DROP TABLE IF EXISTS mentor_audit;
+CREATE TABLE mentor_audit(
+                             id INT GENERATED ALWAYS AS IDENTITY,
+                             mentor_id INT not null,
+                             last_name varchar(40) not null ,
+                             changed_on timestamp(6) not null,
+                             primary key(id)
+);
+
+
+insert into mentors(first_name, last_name) values ('Harold','Finch');
+insert into mentors(first_name, last_name) values ('Severus','Snape');
+
+SELECT * FROM mentors;
+SELECT * FROM mentor_audit;
+
+CREATE OR REPLACE FUNCTION log_last_name_changes()
+    returns trigger
+    language plpgsql
+AS
+$$
+BEGIN
+    IF NEW.last_name <> OLD.last_name THEN
+        INSERT INTO mentor_audit (mentor_id, last_name, changed_on) VALUES
+            (OLD.id,OLD.last_name,now());
+end if;
+RETURN NEW;
+end
+$$;
+
+CREATE TRIGGER last_name_change
+    BEFORE UPDATE
+    ON mentors
+    FOR EACH ROW
+    EXECUTE FUNCTION log_last_name_changes();
+
+UPDATE mentors
+SET last_name = 'XYZ'
+WHERE id = 2;
