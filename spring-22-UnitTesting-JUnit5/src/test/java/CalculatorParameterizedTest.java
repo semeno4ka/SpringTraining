2202d8dc11da18df3acfed0634474f6daa15fe5a
Ekaterina <ekaterina.sementeva@gmail.com>
2022-12-16 10:10:18 -0500

    Test Parameterized Test

--- spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorParameterizedTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 public class CalculatorParameterizedTest {
 
@@ -14,5 +14,24 @@ public class CalculatorParameterizedTest {
   @ValueSource(ints ={3,6,9})//single parameter
   void testCase2(int num) {
     Assertions.assertEquals(0,num %3);//check if all can be divided by 3 with 0 reminder, 3 tests
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings ={"Java","JS","Python"})//single parameter
+//  @EmptySource   //argument becomes "" empty
+//  @NullSource    //argument becomes null
+  @NullAndEmptySource
+  void testCase3(String arg) {
+    Assertions.assertFalse(arg.isEmpty());
+  }
+
+  @ParameterizedTest
+  @MethodSource("stringProvider")
+  void testCase4(String arg) {
+    Assertions.assertFalse(arg.isEmpty());//check each input from array -> 3 parameters, 3 tests
+  }
+
+  static String[] stringProvider(){
+    return new String[]{"Java","JS","TS"};// will run test for each of element
   }
 }
